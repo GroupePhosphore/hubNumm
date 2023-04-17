27c8355b7fb0d37feccea614481d3a8ce669dd4c
PierreGuichardRivalis <pierre@rivalis.fr>
2023-04-17 11:55:17 +0200

    ajout logger

--- src/Controller/InvoiceManagementController.php
+++ src/Controller/InvoiceManagementController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\External\Salesforce\SalesforceClient;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,62 +13,75 @@ use Symfony\Component\Routing\Annotation\Route;
 class InvoiceManagementController extends AbstractController
 {
     #[Route('/invoice/management', name: 'app_invoice_management')]
-    public function updateNumberFromCSVFile(Request $request, SalesforceClient $salesforceClient): JsonResponse
+    public function updateNumberFromCSVFile(Request $request, SalesforceClient $salesforceClient, LoggerInterface $logger): JsonResponse
     {
         $journal = $request->get('journal');
         $entity = $request->get('entity');
         $establishment = $request->get('establishment');
         $file = $request->files->get('file');
 
-        if ($file->getMimeType() !== 'text/csv') {
-            throw new \Exception('File type not allowed');
-        }
-
-        $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
-
-        $savedFile = $file->move($destination);
-
-        $rows = [];
-        if (($fp = fopen($savedFile->getPathname(), 'r')) !== false) {
-            while (($row = fgetcsv($fp)) !== false) {
-                $rows[] = $row;
+        try {
+            if ($file->getMimeType() !== 'text/csv') {
+                throw new \Exception('File type not allowed');
             }
-        }
+        
 
-        unlink($savedFile->getPathname());
+            $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
 
-        if ($rows[0] !== ['DocumentId', 'Numéro', 'Nouveau numéro', 'DateEffet', 'DateCreation']) {
-            throw new \Exception('File columns error');
-        }
+            $savedFile = $file->move($destination);
 
-        array_shift($rows);
-
-        $updatedRows = [];
-        $errorRows = [];
-       foreach($rows as $row) {
-            $date = new \DateTime($row[3]);
-            try {
-                $updated = $salesforceClient->fetch->updateInvoiceBaseLine(
-                    $row[1],
-                    $row[2],
-                    $journal,
-                    $entity,
-                    $establishment,
-                    $date
-                );
-                $updatedRows[] = ['row' => $row];
-            } catch (\Exception $e) {
-                $errorRows[] = [
-                    'row' => $row,
-                    'error' => $e->getMessage()
-                ];
+            $rows = [];
+            if (($fp = fopen($savedFile->getPathname(), 'r')) !== false) {
+                while (($row = fgetcsv($fp)) !== false) {
+                    $rows[] = $row;
+                }
             }
-        }
 
-        return $this->json([
-            'message' => 'Finish',
-            'error' => $errorRows,
-            'success' => $updatedRows
-        ]);
+            unlink($savedFile->getPathname());
+
+            if ($rows[0] !== ['DocumentId', 'Numéro', 'Nouveau numéro', 'DateEffet', 'DateCreation']) {
+                throw new \Exception('File columns error');
+            }
+
+            array_shift($rows);
+
+            $updatedRows = [];
+            $errorRows = [];
+            foreach ($rows as $row) {
+                $date = new \DateTime($row[3]);
+                try {
+                    $updated = $salesforceClient->fetch->updateInvoiceBaseLine(
+                        $row[1],
+                        $row[2],
+                        $journal,
+                        $entity,
+                        $establishment,
+                        $date
+                    );
+                    $updatedRows[] = ['row' => $row];
+                } catch (\Exception $e) {
+                    $errorRows[] = [
+                        'row' => $row,
+                        'error' => $e->getMessage()
+                    ];
+                }
+            }
+
+            $logger->info('Numero de factures - Synchro finie: ');
+            $logger->info('Erreurs : ' . json_encode($errorRows));
+            $logger->info('Succes : ' . json_encode($updatedRows));
+
+            return $this->json([
+                'message' => 'Finish',
+                'error' => $errorRows,
+                'success' => $updatedRows
+            ]);
+        } catch (\Exception $e) {
+            $logger('Error : ' . $e->getMessage());
+            return $this->json([
+                'message' => 'Error',
+                'details' => $e->getMessage()
+            ], 400);
+        }
     }
 }
