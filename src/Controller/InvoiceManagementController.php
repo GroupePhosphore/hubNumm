8ba8d579b5f36accc5d254324d19a1ee945fab92
PierreGuichardRivalis <pierre@rivalis.fr>
2023-05-30 11:12:04 +0200

    Add transfer between entities

--- src/Controller/InvoiceManagementController.php
+++ src/Controller/InvoiceManagementController.php
@@ -7,13 +7,19 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api')]
 class InvoiceManagementController extends AbstractController
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    # A utiliser pour modifier les numeros de facture en cas de probleme avec les imports des factures depuis Henrri
     #[Route('/invoice/management', name: 'app_invoice_management')]
-    public function updateNumberFromCSVFile(Request $request, SalesforceClient $salesforceClient, LoggerInterface $logger): JsonResponse
+    public function updateNumberFromCSVFile(Request $request, SalesforceClient $salesforceClient): JsonResponse
     {
         $journal = $request->get('journal');
         $entity = $request->get('entity');
@@ -21,28 +27,18 @@ class InvoiceManagementController extends AbstractController
         $file = $request->files->get('file');
 
         try {
-            if ($file->getMimeType() !== 'text/csv') {
-                throw new \Exception('File type not allowed');
-            }
-        
-
-            $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
-
-            $savedFile = $file->move($destination);
-
+            $savedFile = $this->saveFile($file);
             $rows = [];
             if (($fp = fopen($savedFile->getPathname(), 'r')) !== false) {
                 while (($row = fgetcsv($fp)) !== false) {
                     $rows[] = $row;
                 }
             }
-
             unlink($savedFile->getPathname());
 
             if ($rows[0] !== ['DocumentId', 'Numéro', 'Nouveau numéro', 'DateEffet', 'DateCreation']) {
                 throw new \Exception('File columns error');
             }
-
             array_shift($rows);
 
             $updatedRows = [];
@@ -67,9 +63,9 @@ class InvoiceManagementController extends AbstractController
                 }
             }
 
-            $logger->info('Numero de factures - Synchro finie: ');
-            $logger->info('Erreurs : ' . json_encode($errorRows));
-            $logger->info('Succes : ' . json_encode($updatedRows));
+            $this->logger->info('Numero de factures - Synchro finie: ');
+            $this->logger->info('Erreurs : ' . json_encode($errorRows));
+            $this->logger->info('Succes : ' . json_encode($updatedRows));
 
             return $this->json([
                 'message' => 'Finish',
@@ -77,11 +73,97 @@ class InvoiceManagementController extends AbstractController
                 'success' => $updatedRows
             ]);
         } catch (\Exception $e) {
-            $logger('Error : ' . $e->getMessage());
+            $this->logger->error('Error : ' . $e->getMessage());
             return $this->json([
                 'message' => 'Error',
                 'details' => $e->getMessage()
             ], 400);
         }
+    }
+
+
+    #[Route('/invoice/switchEntity', name: 'app_invoice_switchEntity')]
+    public function switchBetweenEntities(Request $request, SalesforceClient $client)
+    {
+        $rows = [];
+        $updatedRows = [];
+        $errorRows = [];
+
+        $file = $request->files->get('file');
+        $destinationEntity = $request->get('destinationEntity');
+        $destinationEstablishment = $request->get('destinationEstablishment');
+        $destinationJournal = $request->get('destinationJournal');
+
+        try {
+            $savedFile = $this->saveFile($file);
+
+            if (!($fp = fopen($savedFile->getPathname(), 'r'))) {
+                throw new \Exception("Could not read file", 1);
+            }
+
+            while (($row = fgetcsv($fp, null, ';')) !== false) {
+                try {
+                    $rows[] = $this->parseSingleRow($row, $destinationEntity, $destinationJournal, $destinationEstablishment);
+                } catch (\Exception $e) {
+                    $this->logger->error('Import error - IMPINV2 <br> ----------------------------------<br>' .$e->getMessage() . '<br> ----------------------------------<br>' .json_encode($row));
+                }
+            }
+
+            fclose($fp);
+            unlink($savedFile->getPathname());
+
+            array_shift($rows);
+
+            foreach ($rows as $row) {
+                if (!$updatedRow = $client->fetch->findLineId($row)) {
+                    $errorRows[] = $row;
+                    continue;
+                }
+                $updatedRows[] = $updatedRow;
+            }
+
+            return $this->json([
+                'error' => $errorRows,
+                'success' => $updatedRow
+            ]);
+
+        } catch (\Exception $e) {
+            $this->logger->error('Import error - IMPINV1<br> ----------------------------------<br>' .$e->getMessage() . '<br> ----------------------------------<br>');
+            return $this->json([
+                'error' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    private function parseSingleRow(array $row, string $destinationEntity, string $destinationJournal, string $destinationEstablishment)
+    {
+        return [
+            'entity' => $row[0],
+            'establishment' => $row[1],
+            'journal' => $row[2],
+            'account' => $row[4],
+            'thirdParty' => $row[5],
+            'date' => $row[6],
+            'label' => $row[10],
+            'piece' => $row[9],
+            'reference'=> $row[11],
+            'amount_debit'=> $row[13],
+            'amount_credit'=> $row[14],
+            'destinationJournal' => $destinationJournal,
+            'destinationEntity' => $destinationEntity,
+            'destinationEstablishment' => $destinationEstablishment,
+        ];
+    }
+
+    private function saveFile($file)
+    {
+        $fileIsCSV = $file->getMimeType() === 'text/csv' || ($file->getMimeType() === 'text/plain' && substr($file->getClientOriginalName(), -4, 4) === '.csv');
+        if (!$fileIsCSV) {
+            throw new \Exception('File type not allowed');
+        }
+
+        $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
+        $savedFile = $file->move($destination);
+        return $savedFile;
     }
 }
